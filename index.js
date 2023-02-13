1446aebd6d154bdf526d49b57f42bed492fcd245
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2023-02-13 19:29:13 +0530

    express setup

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Creating an express application
+const app = express();
+
+// Handling json body request 
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json({
+    status: true,
+    message: "Loans API running successfull"
+  });
+});
+
+
+// POST API for new loan application
+app.post('/new-loan', function (req, res) {
+  const loanData = req.body;
+
+  // const firstName = loanData.firstName;
+  // const lastName = loanData.lastName;
+  // const amount = loanData.amount;
+  // const purpose = loanData.purpose;
+  // const email = loanData.email;
+
+  // You can do same thing using destructing object
+  const { firstName, lastName, amount, purpose, email } = loanData;
+
+  if (!firstName) {
+    // return res.status(400).json({
+    //   status: false,
+    //   error: 'Please provide firstname'
+    // })
+    return sendErrorResponse(res, 'Please provide firstname')
+  }
+
+  if(!lastName) {
+    // return res.status(400).json({
+    //   status: false,
+    //   error: 'Please provide lastname'
+    // })
+    return sendErrorResponse(res, 'Please provide lastname')
+  }
+
+  if(!amount) {
+    // return res.status(400).json({
+    //   status: false,
+    //   error: 'Please provide loan amount'
+    // });
+    return sendErrorResponse(res, 'Please provide loan amount')
+    
+  }
+
+  if(!email) {
+    return sendErrorResponse(res, 'Please provide email address')
+  }
+
+  if(!purpose) {
+    return sendErrorResponse(res, 'Please define purpose of applying loan')
+  }
+
+
+  res.json({
+    status: true,
+    message: "New Loan application created...",
+    data: loanData
+  });
+});
+
+
+function sendErrorResponse(response, errorMessage) {
+  return response.status(400).json({
+    status: false,
+    error: errorMessage
+  });
+}
+
+app.listen(3000, function () {
+  console.log(`API Services are running on http://localhost:3000`);
+});
